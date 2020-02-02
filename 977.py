18af5e5733cf3be7a3445b08663ba60e60d13d44
c-yan <c-yan@users.noreply.github.com>
2020-02-02 20:17:17 +0900

    977 新規コード

---
+++ 977.py
@@ -0,0 +1,39 @@
+from sys import setrecursionlimit
+
+
+def find(parent, i):
+    t = parent[i]
+    if t < 0:
+        return i
+    t = find(parent, t)
+    parent[i] = t
+    return t
+
+
+def unite(parent, i, j):
+    i = find(parent, i)
+    j = find(parent, j)
+    if i == j:
+        return
+    parent[j] += parent[i]
+    parent[i] = j
+
+
+setrecursionlimit(10 ** 5)
+
+N = int(input())
+
+parent = [-1] * N
+edges = [0] * N
+for _ in range(N - 1):
+    u, v = map(int, input().split())
+    unite(parent, u, v)
+    edges[u] += 1
+    edges[v] += 1
+
+t = sum(1 for e in parent if e < 0)
+
+if t >= 3 or (t == 2 and 1 in edges):
+    print('Alice')
+else:
+    print('Bob')
