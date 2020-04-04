0359bc5728551865107f611d55946fcfda6aeb8e
c-yan <c-yan@users.noreply.github.com>
2020-04-05 03:59:25 +0900

    1015 ソースコードの改善

--- yc242/1015-2.py
+++ yc242/1015-2.py
@@ -3,42 +3,27 @@ from heapq import heapify, heappop, heappush
 N, X, Y, Z = map(int, input().split())
 A = list(map(int, input().split()))
 
-A = [-a for a in A]
+numbers = [Z, Y, X]
+bills = [10000, 5000, 1000]
+
+A = [-n for n in A]
 heapify(A)
 
-while A:
-    if Z == 0:
-        break
-    x = -heappop(A)
-    if x >= 10000:
-        t = min(x // 10000, Z)
-        Z -= t
-        x -= 10000 * t
-        heappush(A, -x)
-    else:
-        Z -= 1
+for i in range(3):
+    n = numbers[i]
+    b = bills[i]
 
-while A:
-    if Y == 0:
-        break
-    x = -heappop(A)
-    if x >= 5000:
-        t = min(x // 5000, Y)
-        Y -= t
-        x -= 5000 * t
-        heappush(A, -x)
-    else:
-        Y -= 1
-
-while A:
-    if X == 0:
-        break
-    x = -heappop(A)
-    t = min((x + 1000) // 1000, X)
-    X -= t
-    x -= t * 1000
-    if x >= 0:
-        heappush(A, -x)
+    while A:
+        if n == 0:
+            break
+        x = -heappop(A)
+        if x >= b:
+            t = min(x // b, n)
+            n -= t
+            x -= b * t
+            heappush(A, -x)
+        else:
+            n -= 1
 
 if len(A) == 0:
     print('Yes')
