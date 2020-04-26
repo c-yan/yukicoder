d06fcc8fe9dbe0361464e360e7439f0c5dfac4dc
c-yan <c-yan@users.noreply.github.com>
2020-04-26 18:04:25 +0900

    1035 別解

---
+++ yc245/1035-1.py
@@ -0,0 +1,27 @@
+from sys import setrecursionlimit
+
+setrecursionlimit(1000000)
+
+N, M = map(int, input().split())
+
+fac = [0] * (M + 1)
+fac[0] = 1
+for i in range(M):
+    fac[i + 1] = fac[i] * (i + 1) % 1000000007
+
+
+def mcomb(n, k):
+    if n == 0 and k == 0:
+        return 1
+    if n < k or k < 0:
+        return 0
+    return fac[n] * pow(fac[n - k], 1000000005, 1000000007) * pow(fac[k], 1000000005, 1000000007) % 1000000007
+
+
+def f(i):
+    if i == 0:
+        return 0
+    return (mcomb(M, i) * pow(i, N, 1000000007) - f(i - 1)) % 1000000007
+
+
+print(f(M))
