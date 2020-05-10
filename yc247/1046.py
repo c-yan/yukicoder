d2f654ffa41cf35b81eceddc96a9885a68e09609
c-yan <c-yan@users.noreply.github.com>
2020-05-11 03:08:51 +0900

    1046 ソースコードの改善

--- yc247/1046.py
+++ yc247/1046.py
@@ -1,12 +1,5 @@
 N, K = map(int, input().split())
 A = list(map(int, input().split()))
 
-B = [a for a in A if a > 0]
-
-if len(B) != 0:
-    B.sort(reverse=True)
-    print(sum(B[:K]))
-else:
-    B = [a for a in A if a <= 0]
-    B.sort(reverse=True)
-    print(B[0])
+A.sort(reverse=True)
+print(A[0] + sum([a for a in A[1:] if a > 0][:K - 1]))
