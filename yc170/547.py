2401eede47b6f33ac72636f180de3cf40153578d
c-yan <c-yan@users.noreply.github.com>
2020-06-09 00:48:22 +0900

    547 新規コード

---
+++ yc170/547.py
@@ -0,0 +1,10 @@
+N = int(input())
+S = input().split()
+T = input().split()
+
+for i in range(N):
+    if S[i] != T[i]:
+        print(i + 1)
+        print(S[i])
+        print(T[i])
+        break
