61004f823cf07ca818601b2e0088240dcd842e15
c-yan <c-yan@users.noreply.github.com>
2020-06-20 08:20:15 +0900

    1081 ソースコードの改善

--- yc253/1081.py
+++ yc253/1081.py
@@ -1,10 +1,12 @@
 N = int(input())
 A = list(map(int, input().split()))
 
+m = 1000000007
+
 fac = [0] * N
 fac[0] = 1
 for i in range(N - 1):
-    fac[i + 1] = fac[i] * (i + 1) % 1000000007
+    fac[i + 1] = fac[i] * (i + 1) % m
 
 
 def mcomb(n, k):
@@ -12,11 +14,11 @@ def mcomb(n, k):
         return 1
     if n < k or k < 0:
         return 0
-    return fac[n] * pow(fac[n - k], 1000000005, 1000000007) * pow(fac[k], 1000000005, 1000000007) % 1000000007
+    return fac[n] * pow(fac[n - k], m - 2, m) * pow(fac[k], m - 2, m) % m
 
 
 result = 0
 for i in range(N):
     result += mcomb(N - 1, i) * A[i]
-    result %= 1000000007
+    result %= m
 print(result)
