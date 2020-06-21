cd912fdc8815fd5aa7c9f5ceb64997f97f6d394a
c-yan <c-yan@users.noreply.github.com>
2020-06-21 09:34:52 +0900

    all 998244353 の直値を使うのを止めて変数 m を使うよう統一

--- yc250/1066.py
+++ yc250/1066.py
@@ -3,14 +3,16 @@ N, Q = map(int, input().split())
 A = list(map(int, input().split()))
 B = list(map(int, input().split()))
 
+m = 998244353
+
 dp = [0] * (N + 1)
 dp[1] = 1
 dp[0] = A[0] - 1
 for i in range(1, N):
     for j in range(i, -1, -1):
         dp[j + 1] += dp[j]
-        dp[j + 1] %= 998244353
+        dp[j + 1] %= m
         dp[j] *= A[i] - 1
-        dp[j] %= 998244353
+        dp[j] %= m
 
 print('\n'.join(str(dp[b]) for b in B))
