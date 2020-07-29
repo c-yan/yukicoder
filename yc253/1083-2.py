25432427936d2b333c8357df676890e95fe59226
c-yan <c-yan@users.noreply.github.com>
2020-07-30 04:29:14 +0900

    1083 分析用の出力が足されていて通らないコードだったので修正

--- yc253/1083-2.py
+++ yc253/1083-2.py
@@ -5,16 +5,12 @@ A = list(map(int, input().split()))
 
 A.sort(reverse=True)
 
-c = 0
 result = 0
 for p in product([True, False], repeat=N - 1):
     t = K
     for i in range(N - 1):
         if p[i]:
-            c += 1
             t %= A[i]
-    c += 1
     t %= A[N - 1]
     result = max(result, t)
 print(result)
-print(c)
