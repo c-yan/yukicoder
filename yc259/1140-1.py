6e77f11c05bba0210e5a85824eec17ff8b425e7b
c-yan <c-yan@users.noreply.github.com>
2020-08-03 05:02:14 +0900

    1140 高速化

--- yc259/1140-1.py
+++ yc259/1140-1.py
@@ -2,10 +2,12 @@ def make_prime_table(n):
     sieve = [True] * (n + 1)
     sieve[0] = False
     sieve[1] = False
-    for i in range(2, int(n ** 0.5) + 1):
+    for i in range(4, n + 1, 2):
+        sieve[i] = False
+    for i in range(3, int(n ** 0.5) + 1, 2):
         if not sieve[i]:
             continue
-        for j in range(i * i, n + 1, i):
+        for j in range(i * i, n + 1, i * 2):
             sieve[j] = False
     return sieve
 
