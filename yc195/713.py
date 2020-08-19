979690cc2c852a253d57fcd66853fbbd10b33aad
c-yan <c-yan@users.noreply.github.com>
2020-08-20 00:40:27 +0900

    713 新規コード

---
+++ yc195/713.py
@@ -0,0 +1,23 @@
+def make_prime_table(n):
+    sieve = list(range(n + 1))
+    sieve[0] = -1
+    sieve[1] = -1
+    for i in range(4, n + 1, 2):
+        sieve[i] = 2
+    for i in range(3, int(n ** 0.5) + 1, 2):
+        if sieve[i] != i:
+            continue
+        for j in range(i * i, n + 1, i * 2):
+            if sieve[j] == j:
+                sieve[j] = i
+    return sieve
+
+
+N = int(input())
+
+prime_table = make_prime_table(N)
+result = 0
+for i in range(2, N + 1):
+    if prime_table[i] == i:
+        result += i
+print(result)
