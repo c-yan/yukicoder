e06403895784e4f4c8bd1b1370579661281868cb
c-yan <c-yan@users.noreply.github.com>
2020-10-27 07:16:50 +0900

    442 新規コード

---
+++ yc153/442.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func gcd(x, y int) int {
+	if x < y {
+		x, y = y, x
+	}
+	for y > 0 {
+		x, y = y, x%y
+	}
+	return x
+}
+
+func main() {
+	defer flush()
+
+	A := readInt()
+	B := readInt()
+
+	t := gcd(A+B, A)
+	println(t * gcd((A+B)/t, B))
+}
+
+const (
+	ioBufferSize = 1 * 1024 * 1024 // 1 MB
+)
+
+var stdinScanner = func() *bufio.Scanner {
+	result := bufio.NewScanner(os.Stdin)
+	result.Buffer(make([]byte, ioBufferSize), ioBufferSize)
+	result.Split(bufio.ScanWords)
+	return result
+}()
+
+func readString() string {
+	stdinScanner.Scan()
+	return stdinScanner.Text()
+}
+
+func readInt() int {
+	result, err := strconv.Atoi(readString())
+	if err != nil {
+		panic(err)
+	}
+	return result
+}
+
+var stdoutWriter = bufio.NewWriter(os.Stdout)
+
+func flush() {
+	stdoutWriter.Flush()
+}
+
+func println(args ...interface{}) (int, error) {
+	return fmt.Fprintln(stdoutWriter, args...)
+}
---
+++ yc153/442.py
@@ -0,0 +1,5 @@
+from math import gcd
+
+A, B = map(int, input().split())
+
+print(gcd(A + B, A * B))
