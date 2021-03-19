1d0b71b552b60ea47320b815e260c659e613a5d8
c-yan <c-yan@users.noreply.github.com>
2021-03-20 06:01:40 +0900

    1433 ソースコードの改善

--- yuc287/1433.py
+++ yuc287/1433.py
@@ -1,18 +1,16 @@
-from itertools import accumulate
-
 N = int(input())
 S = input()
 a = list(map(int, input().split()))
 
-for i in range(N):
-    if S[i] == 'R':
-        continue
-    a[i] = -a[i]
-
 result = 0
 minv = 0
 maxv = 0
-for v in list(accumulate(a)):
+v = 0
+for i in range(N):
+    if S[i] == 'R':
+        v += a[i]
+    elif S[i] == 'B':
+        v -= a[i]
     result = max(result, abs(v - minv))
     result = max(result, abs(v - maxv))
     minv = min(minv, v)
