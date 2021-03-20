6c5f17f7acb5715449a35579689193ba47cfce8a
c-yan <c-yan@users.noreply.github.com>
2021-03-20 19:49:22 +0900

    1416 ソースコードの改善

--- yc285/1416.py
+++ yc285/1416.py
@@ -1,15 +1,10 @@
+from math import log2
+
 n, *A = map(int, open(0).read().split())
 
 A.sort(reverse=True)
 
 result = 0
-i = 0
-for k in range(100):
-    for j in range(2 ** k):
-        result += k * A[i]
-        i += 1
-        if i == n:
-            break
-    if i == n:
-        break
+for i in range(n):
+    result += int(log2(i + 1)) * A[i]
 print(result)
