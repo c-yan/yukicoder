fb0aefbedee5aaa306438047bdad3cda45fdc98d
c-yan <c-yan@users.noreply.github.com>
2021-03-28 11:58:46 +0900

    1439 ソースコードの改善

--- yc288/1439.py
+++ yc288/1439.py
@@ -1,4 +1,5 @@
 from sys import stdin
+from heapq import heapify, heappop, heappush
 
 readline = stdin.readline
 
@@ -7,27 +8,41 @@ S = list(readline()[:-1])
 T = list(readline()[:-1])
 Q = int(readline())
 
-p = 0
-while p < N and S[p] == T[p]:
-    p += 1
+s = set()
+q = []
+for i in range(N):
+    if S[i] == T[i]:
+        continue
+    q.append(i)
+    s.add(i)
+heapify(q)
 
 result = []
 for _ in range(Q):
     c, x, y = readline().split()
     x = int(x) - 1
-    if  c == 'S':
+
+    is_same = S[x] == T[x]
+    if c == 'S':
         S[x] = y
     elif c == 'T':
         T[x] = y
-    if x < p and S[x] != T[x]:
-        p = x
-    elif x == p and S[x] == T[x]:
-        while p < N and S[p] == T[p]:
-            p += 1
+    if (S[x] == T[x]) != is_same:
+        if is_same:
+            heappush(q, x)
+            s.add(x)
+        else:
+            s.remove(x)
 
-    if p == N:
+    while len(q) != 0 and q[0] not in s:
+        heappop(q)
+
+    if len(q) == 0:
         result.append('=')
-    elif S[p] > T[p]:
+        continue
+
+    p = q[0]
+    if S[p] > T[p]:
         result.append('>')
     elif S[p] < T[p]:
         result.append('<')
